f1eaf4dadc63f87cf2327b0d478f166360539c17
TenesacaJ <mewtwo9594@gmail.com>
2024-04-24 12:03:09 -0500

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Database, onValue, ref } from '@angular/fire/database';
+import { LocalNotifications } from '@capacitor/local-notifications';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private database: Database) {
+    await LocalNotifications.requestPermissions(); // Solicitar permisos de la app
+
+  }
+
+  ngOnInit() {
+    const route = ref(this.database, '/tiempo'); // Ruta directa a donde se encuentra el número
+
+    onValue(route, (snapshot) => {
+      const numero = snapshot.val(); // Obtén el valor numérico de la consulta
+      const background = document.getElementById('background');
+
+      if (background) {
+        background.classList.add('fade-out'); // Agrega la clase para desvanecer la imagen actual
+        setTimeout(async () => {
+          // Cambia la imagen después de que la imagen actual se haya desvanecido
+          if (numero >= 0 && numero <= 1000) {
+            background.style.backgroundImage = "url(/assets/img/noche.jpg)";
+            await this.enviarNotificacion("¡Buenas noches!", "Es hora de descansar");
+          } else if (numero > 2500) {
+            background.style.backgroundImage = "url(/assets/img/dia.jpg)";
+            await this.enviarNotificacion("Buenos días", "Es hora de empezar el día");
+          } else {
+            background.style.backgroundImage = "url(/assets/img/tarde.jpg)";
+            await this.enviarNotificacion("¡Buenas tardes!", "Es hora de aprovechar la tarde");
+          }
+          background.classList.remove('fade-out'); // Elimina la clase de desvanecimiento para mostrar la nueva imagen
+        }, 500); // Ajusta este valor al mismo que la duración de la transición en CSS
+      }
+    });
+  }
+
+  async enviarNotificacion(title: string, body: string) {
+  await LocalNotifications.schedule({
+    notifications: [
+      {
+        title: title,
+        body: body,
+        id: Math.floor(Math.random() * 1000), // Generar un ID aleatorio para evitar conflictos
+        schedule: {
+          allowWhileIdle: true // Permite que la notificación se ejecute incluso durante el modo Reposo
+        }
+      }
+    ]
+  });
+}
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAmUcQSTukkoqAjdLzrzuPGUo3w1jwzvWY",
+    authDomain: "sumativaap5.firebaseapp.com",
+    databaseURL: "https://sumativaap5-default-rtdb.firebaseio.com",
+    projectId: "sumativaap5",
+    storageBucket: "sumativaap5.appspot.com",
+    messagingSenderId: "771486472547",
+    appId: "1:771486472547:web:c6615d99eb1a5e394f5b56"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
